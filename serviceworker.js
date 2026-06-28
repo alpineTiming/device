1f2d1bd6bb67ed3655df5c5f48189966bd0b2b2b
Cormac Wilson <cw2063@hw.ac.uk>
2026-06-29 00:08:07 +0100

    [code] [media] [docs] added initial content for manifest.json, serviceworker.js and media assets for icons.

---
+++ serviceworker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'alpineTimingApp-cache-v1';
+const ASSETS = [
+  './manifest.json',
+  './assets/images/iconSmall.svg',
+  './assets/images/iconSmall.png',
+  './assets/images/iconLarge.svg',
+  './assets/images/iconLarge.png',
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(() => caches.match('./app.html'))
+  );
+});
